da31342a401e4767ca4144127c9d70be4db18d9d
cpredmond98 <66801934+cpredmond98@users.noreply.github.com>
2021-08-20 11:56:26 -0400

    Update launch_mr_job.sh
    add env vars

--- launch_mr_job.sh
+++ launch_mr_job.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+  
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64
+export PATH=$PATH:JAVA_HOME/bin
+export HADOOP_CLASSPATH=/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64/lib/tools.jar
 
 echo "compiling program"
 hadoop com.sun.tools.javac.Main StockVolatility.java
 jar cf sv.jar StockVolatility*.class
 
 echo "running map reduce"
+hdfs dfs -rm -r stock/mr_output
 hadoop jar sv.jar StockVolatility stock/pig_output stock/mr_output
 
 echo "retrieving map reduce results"
