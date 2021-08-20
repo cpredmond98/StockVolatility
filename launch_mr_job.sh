55f5ef34ad1516cd97d9fea5052ea1a8ba8f2f20
cpredmond98 <66801934+cpredmond98@users.noreply.github.com>
2021-08-20 11:11:45 -0400

    Update launch_mr_job.sh

--- launch_mr_job.sh
+++ launch_mr_job.sh
@@ -4,11 +4,11 @@ echo "compiling program"
 hadoop com.sun.tools.javac.Main StockVolatility.java
 jar cf sv.jar StockVolatility*.class
 
-echo "running"
+echo "running map reduce"
 hadoop jar sv.jar StockVolatility stock/pig_output stock/mr_output
 
-echo "retrieving results"
+echo "retrieving map reduce results"
 rm -r ~/stock/output
-hdfs dfs -get stock/output ~/stock/output
+hdfs dfs -get stock/mr_output ~/stock/output
 hdfs dfs -rm -r stock/output
 cat ~/stock/output/*
