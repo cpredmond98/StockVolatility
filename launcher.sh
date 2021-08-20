07574cf4fd4f7570bddaace0aa8761517ba5ff79
Johan Terblanche <johan.h.terblanche1@gmail.com>
2021-08-20 10:00:16 -0500

    Add files via upload

---
+++ launcher.sh
@@ -0,0 +1,18 @@
+
+echo "Starting script."
+
+echo "Running Hive queries, will print the 10 days with the highest volume of trades."
+
+hive -f hive_queries.sql
+
+echo "Preparing to run pig"
+hdfs dfs -rm -r stock/output
+echo "Running pig"
+pig -x -useHCatalog mapreduce pig_queries.pig
+
+rm -r pig_output
+hdfs dfs -copyToLocal stocks pig_output
+
+echo "displaying first 5 rows of the table generated in Hive/Pig"
+head pig_output/*
+
