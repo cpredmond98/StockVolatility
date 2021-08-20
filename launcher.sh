38e82054e7c84d63599e1827163e22cde8c68c7b
Johan Terblanche <johan.h.terblanche1@gmail.com>
2021-08-20 11:24:08 -0500

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -6,7 +6,7 @@ echo "Running Hive queries, will print the 10 days with the highest volume of tr
 hive -f hive_queries.sql
 
 echo "Running Hive query to print the 5 year max and 5 year min into a text file named '5_yearminmax.txt'"
-hive -e 'select stockName, FY_Max, FY_Min from processed' > 5_yearminmix.txt
+hive -e 'select stockName, FY_Max, FY_Min from stock.processed' > 5_yearminmix.txt
 
 echo "Preparing to run pig"
 hdfs dfs -rm -r stock/pig_output
